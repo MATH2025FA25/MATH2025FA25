1e922f44c1c930025cf968e0035e1e82a19d8efc
EricFriedlander <EricF2218@gmail.com>
2025-08-26 16:08:18 -0600

    Fixing syllabus

---
+++ GenerateSticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+library(tidyverse)
+
+p <- ggplot(aes(x = mpg, y = -wt), data = mtcars) + geom_point(size = 0.5, color = "#ffffff") +
+  geom_smooth(method="lm", size=0.5, color="#a49363", se=FALSE)
+p <- p + theme_void() + theme_transparent()
+ 
+
+sticker(p, package="MATH 2025", p_size=20, s_x=1, s_y=0.90, s_width=1.3, s_height=0.9,
+        h_fill="#412d5e", h_color = "#a49363",
+        p_color="#bbbbbb",
+        filename="images/sticker212.png")
+
